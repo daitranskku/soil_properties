dfd11011564b23948af075192fb76458b9077558
daitranskku <daitran@skku.edu>
2021-04-15 17:02:04 +0900

    Update 3D

--- 3D.py
+++ 3D.py
@@ -4,20 +4,32 @@ import pandas as pd
 from collections import Counter
 import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
 
 from sklearn import preprocessing
 import tensorflow as tf
 import tensorflow_probability as tfp
 
+
+X_TEST_START = 549500
+X_TEST_STOP = 551500
+X_STEP = 50
+
+Y_TEST_START = 213550
+Y_TEST_STOP = 213950
+Y_STEP = 50
+
+ELEVATION_START = -20
+ELEVATION_STOP = 44
+ELEVATION_STEP = 5
+
 DATA_DIR = '/home/daitran/Desktop/git/soil_properties/data'
+
 ntb_data_path = os.path.join(DATA_DIR, 'NTB_black_data.csv')
 tb_data_path = os.path.join(DATA_DIR, 'TB_blue_data.csv')
 
 ntb_data= pd.read_csv(ntb_data_path)
 tb_data = pd.read_csv(tb_data_path)
-
-ntb_names = ntb_data['Soi properties (main)']
-tb_names = tb_data['Soi properties (main)']
 
 assign_num_list = {'topsoil layer': 0,
                       'weathered rock': 1,
@@ -29,13 +41,15 @@ assign_num_list = {'topsoil layer': 0,
                       'sedimentary layer': 7,
                       'reclaimed layer': 8}
 
+
+ntb_names = ntb_data['Soi properties (main)']
+tb_names = tb_data['Soi properties (main)']
+
 ntb_target = ntb_names.replace(assign_num_list)
 tb_target = tb_names.replace(assign_num_list)
 
-
 ntb_data['Target'] = ntb_target
 tb_data['Target'] = tb_target
-
 
 X_train_ntb = ntb_data[['X','Y','Elevation']].to_numpy()
 y_train_ntb = ntb_data['Target'].to_numpy()
@@ -44,23 +58,55 @@ X_train_tb = tb_data[['X','Y','Elevation']].to_numpy()
 y_train_tb = tb_data['Target'].to_numpy()
 
 # https://scikit-learn.org/stable/modules/preprocessing.html
-# Minmax scaler
-# Standart Scaler
-# 
-
 normalizer = preprocessing.MinMaxScaler()
 
 normalized_X_train_ntb = normalizer.fit_transform(X_train_ntb)
 normalized_X_train_tb = normalizer.fit_transform(X_train_tb)
-# normalized_train_X
 
 
-labels = {}
-for k, v in assign_num_list.items():
-    labels[v] = k
-label_colours = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'brown', 'pink']
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import RMSprop
+
+
+model = Sequential()
+model.add(Dense(10, input_dim=3, kernel_initializer='normal', activation='relu'))
+model.add(Dense(10, input_dim=3, kernel_initializer='normal', activation='relu'))
+model.add(Dense(10, input_dim=3, kernel_initializer='normal', activation='relu'))
+model.add(Dense(1, kernel_initializer='normal', activation='linear'))
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(normalized_X_train_ntb, y_train_ntb, 
+          epochs = 1000, verbose=1)
+
+def test_area_generate():
+    test_area = np.empty((0,3), int)
+    row = np.array([])
+    for x in tqdm(range(X_TEST_START, X_TEST_STOP,X_STEP)):
+        for y in range(Y_TEST_START, Y_TEST_STOP,Y_STEP):
+            for elevation in range(ELEVATION_START, ELEVATION_STOP, ELEVATION_STEP):
+                row = np.array([x,y,elevation])
+                row = np.expand_dims(row,axis=0)
+    #             print(row.shape)
+                test_area = np.append(test_area, row, axis = 0)
+#             print('Locationx: {}, y: {}, elevation: {}'.format(x,y,elevation))
+    return test_area
+
+
+X_test = test_area_generate()
+normalized_X_test = normalizer.fit_transform(X_test)
+
+def estimate_soil_properties(test_area):
+    results = []
+    for i in tqdm(range(len(test_area))):
+        test_location = normalized_X_test[i]
+        test_location = np.expand_dims(test_location, axis =0)
+        result = model.predict(test_location)
+        results.append(int(result))
+    return results
+y_estimate = estimate_soil_properties(test_area = X_test)
 def plot_data(x, y, labels, colours):
-    fig = plt.figure(figsize=(8, 8))
+    fig = plt.figure(figsize=(10,10))
     ax = fig.add_subplot(projection='3d')
     for c in np.unique(y):
         inx = np.where(y == c)
@@ -71,6 +117,19 @@ def plot_data(x, y, labels, colours):
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
           fancybox=True, shadow=True, ncol=5)
     
+labels = {}
+for k, v in assign_num_list.items():
+    labels[v] = k
+label_colours = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'brown', 'pink']
 
-plot_data(normalized_X_train_ntb, y_train_ntb, labels, label_colours)
+
+merged_X = []
+merged_y = []
+
+merged_X = np.append(X_train_ntb, X_test, axis = 0)
+merged_y = np.append(y_train_ntb, y_estimate, axis = 0)
+
+# merged_X = np.append(merged_X, X_train_tb, axis = 0)
+# merged_y = np.append(merged_y, y_train_tb, axis = 0)
+plot_data(merged_X, merged_y, labels, label_colours)
 plt.show()
